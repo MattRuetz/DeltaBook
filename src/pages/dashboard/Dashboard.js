035400362b07cdaa9d7ca8e4007cb14e4aa36b71
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-26 19:56:28 -0400

    fix warning

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useCollection } from '../../hooks/useCollection';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import ProjectList from '../../components/ProjectList';
